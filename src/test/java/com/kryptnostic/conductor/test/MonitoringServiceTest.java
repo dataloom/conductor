44c8bc66d50beb822b8c723e390ab3108e8e9d2d
Yao <yao@kryptnostic.com>
2015-11-18 11:09:31 -0800

    test commit

---
+++ src/test/java/com/kryptnostic/conductor/test/MonitoringServiceTest.java
@@ -0,0 +1,14 @@
+package com.kryptnostic.conductor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonitoringServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
