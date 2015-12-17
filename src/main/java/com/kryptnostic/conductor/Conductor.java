43fa2b57a704f067f12f0908e89850e3d489109b
Matthew Tamayo-Rios <mtamayo@mjolnir.local>
2015-12-17 01:28:39 -0800

    Switch to rhizome server base.

--- src/main/java/com/kryptnostic/conductor/Conductor.java
+++ src/main/java/com/kryptnostic/conductor/Conductor.java
@@ -1,66 +1,24 @@
 package com.kryptnostic.conductor;
 
-import org.springframework.beans.BeansException;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-
-import com.geekbeast.rhizome.core.Rhizome;
-import com.geekbeast.rhizome.pods.AsyncPod;
-import com.geekbeast.rhizome.pods.ConfigurationPod;
-import com.geekbeast.rhizome.pods.HazelcastPod;
-import com.geekbeast.rhizome.pods.MetricsPod;
-import com.geekbeast.rhizome.pods.ServletContainerPod;
+import com.geekbeast.rhizome.configuration.websockets.RhizomeServerBase;
 import com.geekbeast.rhizome.pods.hazelcast.RegistryBasedHazelcastInstanceConfigurationPod;
 import com.kryptnostic.conductor.pods.ConductorSecurityPod;
 import com.kryptnostic.conductor.pods.ConductorServicesPod;
 import com.kryptnostic.conductor.pods.ConductorServletsPod;
 import com.kryptnostic.mapstores.pods.SerializersPod;
 
-public class Conductor {
-
-    private final Rhizome rhizome;
-
-    public Conductor() {
-        this( new Class<?>[] {
-                ConfigurationPod.class,
-                MetricsPod.class,
-                AsyncPod.class,
-                HazelcastPod.class,
-                ServletContainerPod.class,
+public class Conductor extends RhizomeServerBase {
+    public Conductor( Class<?>... defaultPods ) {
+        super(
                 RegistryBasedHazelcastInstanceConfigurationPod.class,
-                SerializersPod.class } );
-    }
-
-    public Conductor( Class<?>[] defaultPods ) {
-        this(
-                defaultPods,
+                SerializersPod.class,
                 ConductorServletsPod.class,
                 ConductorServicesPod.class,
                 ConductorSecurityPod.class );
     }
 
-    public Conductor( Class<?>[] defaultPods, Class<?>... pods ) {
-        rhizome = new Rhizome( pods) {
-            @Override
-            public Class<?>[] getDefaultPods( ) {
-                return defaultPods;
-            }
-        };
-    }
-
-    public void start( String... activeProfiles ) throws Exception {
-        rhizome.sprout( activeProfiles );
-    }
-
-    public void stop() throws BeansException, Exception {
-        rhizome.wilt();
-    }
-
     public static void main( String[] args ) throws Exception {
         new Conductor().start( args );
-    }
-
-    public AnnotationConfigWebApplicationContext getContext() {
-        return rhizome.getContext();
     }
 
 }
