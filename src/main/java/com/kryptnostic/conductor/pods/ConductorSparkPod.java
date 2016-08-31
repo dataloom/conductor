40298241824f3b5322733e4c2a8e8b88ef3da956
Yao <yao@kryptnostic.com>
2016-08-31 11:09:15 -0700

    using Rhizome's Cassandra Configuration

--- src/main/java/com/kryptnostic/conductor/pods/ConductorSparkPod.java
+++ src/main/java/com/kryptnostic/conductor/pods/ConductorSparkPod.java
@@ -2,8 +2,7 @@ package com.kryptnostic.conductor.pods;
 
 import com.datastax.spark.connector.japi.CassandraJavaUtil;
 import com.datastax.spark.connector.japi.SparkContextJavaFunctions;
-import com.google.common.base.Optional;
-import com.kryptnostic.rhizome.datacenter.DockerInfoService;
+import com.kryptnostic.rhizome.configuration.cassandra.CassandraConfiguration;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.cassandra.CassandraSQLContext;
@@ -17,31 +16,35 @@ import com.kryptnostic.rhizome.configuration.service.ConfigurationService;
 import com.kryptnostic.sparks.ConductorSparkImpl;
 import com.kryptnostic.sparks.SparkAuthorizationManager;
 
+import javax.inject.Inject;
+
 @Configuration
 public class ConductorSparkPod {
     // TODO: Hack to avoid circular dependency... need to move Spark Jars config into rhizome.yaml
     ConductorConfiguration conductorConfiguration = ConfigurationService.StaticLoader
-            .loadConfiguration( ConductorConfiguration.class );
-    String[]               sparkMasters           = new String[] { "mjolnir:7077", "mjolnir.local:7077",
-            "localhost:7077" };
+            .loadConfiguration(ConductorConfiguration.class);
+    String[] sparkMasters = new String[]{"mjolnir:7077", "mjolnir.local:7077",
+            "localhost:7077"};
+
+    @Inject
+    private CassandraConfiguration cassandraConfiguration;
 
     @Bean
     public SparkConf sparkConf() {
-        StringBuilder sparkMasterUrlBuilder = new StringBuilder( "spark://" );
-        for ( int i = 0; i < sparkMasters.length; ) {
-            sparkMasterUrlBuilder.append( sparkMasters[ i ] ).append( i++ == sparkMasters.length ? "" : "," );
+        StringBuilder sparkMasterUrlBuilder = new StringBuilder("spark://");
+        for (int i = 0; i < sparkMasters.length; ) {
+            sparkMasterUrlBuilder.append(sparkMasters[i]).append(i++ == sparkMasters.length ? "" : ",");
         }
 
-        return new SparkConf().setAppName( "Kryptnostic Spark Conductor" )
-                .setMaster( sparkMasterUrlBuilder.toString() )
-                .setJars( conductorConfiguration.getSparkJars() )
-                .set("spark.cassdra.connection.host",
-                        DockerInfoService.getContainerIPsWithTag( "cassandra", Optional.of( "host" ) ).iterator().next() );
+        return new SparkConf().setAppName("Kryptnostic Spark Conductor")
+                .setMaster(sparkMasterUrlBuilder.toString())
+                .setJars(conductorConfiguration.getSparkJars())
+                .set("spark.cassdra.connection.host", cassandraConfiguration.getCassandraSeedNodes().iterator().next().getHostAddress() );
     }
 
     @Bean
     public JavaSparkContext javaSparkContext() {
-        return new JavaSparkContext( sparkConf() );
+        return new JavaSparkContext(sparkConf());
     }
 
     @Bean
@@ -51,7 +54,7 @@ public class ConductorSparkPod {
 
     @Bean
     public SparkContextJavaFunctions sparkContextJavaFunctions() {
-        return CassandraJavaUtil.javaFunctions( javaSparkContext() );
+        return CassandraJavaUtil.javaFunctions(javaSparkContext());
     }
 
     @Bean
@@ -61,6 +64,6 @@ public class ConductorSparkPod {
                 javaSparkContext(),
                 cassandraSQLContext(),
                 sparkContextJavaFunctions(),
-                new SparkAuthorizationManager() );
+                new SparkAuthorizationManager());
     }
 }
