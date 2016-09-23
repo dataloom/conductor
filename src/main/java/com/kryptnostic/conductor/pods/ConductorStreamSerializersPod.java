ee0c609e904699692bb07c3d97a85a91bb49d37b
Yao <yao@kryptnostic.com>
2016-09-23 10:36:11 -0700

    unblock frontend

--- src/main/java/com/kryptnostic/conductor/pods/ConductorStreamSerializersPod.java
+++ src/main/java/com/kryptnostic/conductor/pods/ConductorStreamSerializersPod.java
@@ -1,27 +1,14 @@
 package com.kryptnostic.conductor.pods;
 
-import javax.inject.Inject;
-
-import com.datastax.driver.core.Session;
 import com.kryptnostic.conductor.rpc.serializers.QueryResultStreamSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.kryptnostic.conductor.rpc.ConductorSparkApi;
 import com.kryptnostic.conductor.rpc.LambdaStreamSerializer;
-import com.kryptnostic.conductor.rpc.serializers.CallableStreamSerializer;
 import com.kryptnostic.conductor.rpc.serializers.ConductorCallStreamSerializer;
-import com.kryptnostic.conductor.rpc.serializers.EmployeeStreamSerializer;
 
 @Configuration
 public class ConductorStreamSerializersPod {
-    @Inject
-    private ConductorSparkApi api;
-
-    @Inject
-
-    private Session session;
-
     @Bean
     public LambdaStreamSerializer lss() {
         return new LambdaStreamSerializer();
@@ -29,11 +16,11 @@ public class ConductorStreamSerializersPod {
 
     @Bean
     public ConductorCallStreamSerializer ccss() {
-        return new ConductorCallStreamSerializer( api );
+        return new ConductorCallStreamSerializer( null );
     }
 
     @Bean
     public QueryResultStreamSerializer qrss() {
-        return new QueryResultStreamSerializer( session );
+        return new QueryResultStreamSerializer( null );
     }
 }
