ee7096d8a5aef99be99a1378a7b8afd50e8f0858
Drew <drew@openlattice.com>
2019-06-25 15:45:24 -0700

    remove dupe Auth0Pod

--- src/main/java/com/openlattice/Conductor.java
+++ src/main/java/com/openlattice/Conductor.java
@@ -44,23 +44,24 @@ import com.openlattice.tasks.pods.TaskSchedulerPod;
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 public class Conductor extends RhizomeApplicationServer {
-    static final Class<?>[] rhizomePods = new Class<?>[] { RegistryBasedHazelcastInstanceConfigurationPod.class };
+    static final Class<?>[] rhizomePods = new Class<?>[] {
+            RegistryBasedHazelcastInstanceConfigurationPod.class,
+            Auth0Pod.class
+    };
 
     static final Class<?>[] conductorPods = new Class<?>[] {
+            AwsS3Pod.class,
+            ByteBlobServicePod.class,
             ConductorPostInitializationPod.class,
             ConductorServicesPod.class,
-            ByteBlobServicePod.class,
             ConductorEdmSyncPod.class,
-            SharedStreamSerializersPod.class,
-            PlasmaCoupling.class,
-            MailServicePod.class,
-            Auth0Pod.class,
-            MapstoresPod.class,
             JdbcPod.class,
-            PostgresTablesPod.class,
+            MailServicePod.class,
+            MapstoresPod.class,
+            PlasmaCoupling.class,
             PostgresPod.class,
-            Auth0Pod.class,
-            AwsS3Pod.class,
+            PostgresTablesPod.class,
+            SharedStreamSerializersPod.class,
             TaskSchedulerPod.class
     };
 
